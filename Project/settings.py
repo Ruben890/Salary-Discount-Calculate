930817613a129581a27df7255ba67d7a53bbd0b2
Ruben890 <chinausto@gmail.com>
2022-10-23 15:09:00 -0400

    cambios Seguridad

--- Project/settings.py
+++ Project/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,10 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lef6ik169k*u_c-vo4tua0jnwsjiugs&!=y)v^r0c_#qde&a_6'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -107,7 +107,6 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-
 
 
 # Default primary key field type
